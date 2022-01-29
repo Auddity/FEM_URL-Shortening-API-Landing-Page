d7f02a5c9683f8582ce59a0b7e230b106eb4d59f
Auddity <audsecondarylap@outlook.com>
2022-01-29 11:37:48 -0500

    xhr outline

--- app.js
+++ app.js
@@ -8,6 +8,35 @@ const menuBtn = getElement('menu-btn');
 const menuModal = getElement('nav-bar-menu');
 const startBtns = document.querySelectorAll('.btn-cta');
 const form = getElement('form');
+const input = getElement('form-input').value;
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  
+  getData();
+});
+
+const getData = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `https://api.shrtco.de/v2/shorten?url=${input}`);
+  console.log(xhr.response); // empty
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      console.log('success');
+    }
+    console.log({
+      status: xhr.status,
+      text: xhr.statusText,
+      state: xhr.readyState
+    });
+  }
+  xhr.send();
+}
+
+const displayData = () => {
+
+};
 
 // Open Menu Modal
 menuBtn.addEventListener('click', () => {
